9b31018aec408a071e7e0fce34ea986b1b8df3c5
Braden Pellett <bpellett@ucdavis.edu>
2013-12-16 14:19:30 -0800

    Add api introspection methods, and simple introspection output for client

--- apiapp.rb
+++ apiapp.rb
@@ -12,6 +12,8 @@ HTTP_BAD_REQUEST = 400
 HTTP_NOT_FOUND = 404
 HTTP_METHOD_NOT_ALLOWED = 405
 
+HTTP_METHODS = [:GET, :POST, :PUT, :DELETE]
+
 class APIApp
   def initialize(api, *api_args, log:Logger.new(STDERR), **api_named_args)
     @log = log
@@ -20,6 +22,12 @@ class APIApp
       "#{time.strftime '%FT%T%z'} #{req.ip} #{[req.referer, req.user_agent].map{|s|(s||'-').inspect}.join(' ')} #{cmd} #{msg}\n"
     }
     @api = api.new(*api_args, log: log, **api_named_args)
+  end
+  def cmds
+    (HTTP_METHODS & @api.class.constants).flat_map{|m| @api.class.const_get(m)} & @api.methods
+  end
+  def cmd_method(cmd)
+    (@api.class.constants & HTTP_METHODS).reduce{|memo,http_method| @api.class.const_get(http_method).include?(cmd) ? http_method : memo}
   end
   def call(env)
     req = Rack::Request.new env
@@ -30,21 +38,33 @@ class APIApp
     }), []] if req.options?
     cmd, args, named_args = (
       _, cmd, *args = req.path_info.split('/').map {|e| Rack::Utils::unescape e}
-      cmd = cmd.to_sym rescue :''
+      cmd = cmd.to_sym rescue nil
       ignore_params = env['HTTP_X_IGNORE_PARAMS'].split(',').map{|p|p.strip} rescue []
       named_args = Hash[req.params.reject{|k,v| ignore_params.include? k}.map{|k,v| [k.to_sym,v]}]
       [cmd, args, named_args]
     )
-    return [HTTP_NOT_FOUND, h, []] if !@api.class.public_method_defined?(cmd)
-    return [HTTP_METHOD_NOT_ALLOWED, h, []] if !(@api.class.const_get(req.request_method.to_sym).include?(cmd) rescue false)
-    Fiber.new do
-      Thread.current[:loginfo] = [req, cmd]
-      begin
-        result = named_args.empty? ? @api.public_send(cmd, *args) : @api.public_send(cmd, *args, **named_args)
-        [HTTP_OK, h.merge({"Content-Type" => "text/plain"}), [result.to_s]]
-      rescue ArgumentError => e
-        [HTTP_BAD_REQUEST, h.merge({"Content-Type" => "text/plain"}), [e.to_s]]
-      end
-    end.resume
+    if cmd == nil
+      [HTTP_NOT_FOUND, h.merge({"Content-Type" => "text/plain"}), [
+        cmds.map{|c|
+          ps = @api.method(c).parameters
+          data = ps.map{|p| (p[0]==:key && "#{p[1]}=") || (p[0]==:keyrest && "...") || nil}.compact.join('&')
+          "#{cmd_method(c)} #{c}/" +
+            ps.map{|p| (p[0]==:req && "#{p[1]}") || (p[0]==:rest && "...") || nil}.compact.join('/') +
+            (data.empty? ? "" : "?#{data}") + "\n"
+        }.join
+      ]]
+    else
+      return [HTTP_NOT_FOUND, h, []] if !cmds.include?(cmd)
+      return [HTTP_METHOD_NOT_ALLOWED, h, []] if req.request_method.to_sym != cmd_method(cmd)
+      Fiber.new do
+        Thread.current[:loginfo] = [req, cmd]
+        begin
+          result = named_args.empty? ? @api.public_send(cmd, *args) : @api.public_send(cmd, *args, **named_args)
+          [HTTP_OK, h.merge({"Content-Type" => "text/plain"}), [result.to_s]]
+        rescue ArgumentError => e
+          [HTTP_BAD_REQUEST, h.merge({"Content-Type" => "text/plain"}), [e.to_s]]
+        end
+      end.resume
+    end
   end
 end
